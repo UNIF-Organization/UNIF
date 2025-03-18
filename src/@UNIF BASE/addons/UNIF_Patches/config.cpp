c4cbdf806db4a11b80bfb6e8227123e98e56e53e
Erik Schulze <erikmail3333@gmail.com>
2025-03-18 20:08:01 +0100

    sync github to the content of the live mod

---
+++ src/@UNIF BASE/addons/UNIF_Patches/config.cpp
@@ -0,0 +1,40 @@
+#include "BIS_AddonInfo.hpp"
+class CfgPatches 
+{
+	class UNIF_Patches 
+	{
+        author = "UNIF Mod Team";
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 1.0;
+		requiredAddons[] = {"A3_Weapons_F"};
+	};
+};
+
+class CfgUnitInsignia 
+{
+	class UNIF_Patch_1 //Classname (cannot be 2 with the same name)
+	{
+		displayName = "[UNIF] RAT"; //How its named ingame
+		texture = "\UNIF_Patches\data\UNIF_base.paa"; //location of the texture
+		author = "UNIF Dev Team"; //I dont think i need to explain this
+	};
+	class UNIF_Patch_2 //Classname (cannot be 2 with the same name)
+	{
+		displayName = "[UNIF] RAT (Pride)"; //How its named ingame
+		texture = "\UNIF_Patches\data\UNIF_Pride.paa"; //location of the texture
+		author = "UNIF Dev Team"; //I dont think i need to explain this
+	};
+	class UNIF_Patch_3 //Classname (cannot be 2 with the same name)
+	{
+		displayName = "[UNIF] UNIF (at Home)"; //How its named ingame
+		texture = "\UNIF_Patches\data\PatchFridge.paa"; //location of the texture
+		author = "UNIF Dev Team"; //I dont think i need to explain this
+	};
+	class UNIF_Patch_4 //Classname (cannot be 2 with the same name)
+	{
+		displayName = "[UNIF] UNIF"; //How its named ingame
+		texture = "\UNIF_Patches\data\UNIF_Intervention.paa"; //location of the texture
+		author = "UNIF Dev Team"; //I dont think i need to explain this
+	};
+};
---
+++ src/@UNIF BASE/meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 3237056386;
+name = "UNIF BASE";
+timestamp = 5250260656314663091;
